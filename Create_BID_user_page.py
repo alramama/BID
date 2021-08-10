a0c52a8efd19b739fbc4a0cf0476df5ee952b2c0
alramama <74817864+alramama@users.noreply.github.com>
2021-08-10 09:50:38 +0300

    Update Create_BID_user_page.py

--- Create_BID_user_page.py
+++ Create_BID_user_page.py
@@ -31,6 +31,20 @@ BID_MainFrame.grid(row=0, column=0)
 FRAM_BID_Emp_Info = Frame(BID_MainFrame, bd=16, width=100, height=80, relief=RIDGE)
 FRAM_BID_Emp_Info.grid(row=1, column=0)
 
+typeOfworks = ["Construct" , "Supply" ,"installation","Testing","commissioning","Add","Preventive maintenance","Corrective maintenance","Maintenance","replace","check","Upgrade"]
+
+Equipment = ["Generators","Transformers","Distribution_Panel","Medium voltage","Breakers","Cable","SCADA_System"]
+
+Generators = ["Backup generators","prime generators","Mobil Generator"]
+Generators_Specification = []
+Transformers = ["Power transformer","Startup transformer","Medium voltage transformer"]
+Distribution_Panel = ["Main distribution panels","Sub-distribution panels"]
+Medium_Voltage = ["Switch gear","Ring Main units"]
+Breakers = ["ACB","MCB","MCCB"]
+Cable = ["Medium voltage cables","Low voltage cables"]
+SCADA_System = ["UPS","RTU","New Readings","Battery charge","Battery bank"]
+Montring_System = ["PDM","DGA","WAMS"]
+
 lbl_work_Order =    Label(FRAM_BID_Emp_Info, font=('arial', 20, 'bold'), text="Work_Order",   fg="black", bd=5).grid(row=1, column=0, sticky=W)
 lbl_rfq_no =        Label(FRAM_BID_Emp_Info, font=('arial', 20, 'bold'), text="RFQ_No",       fg="black", bd=5).grid(row=2, column=0, sticky=W)
 lbl_work_Order =    Entry(FRAM_BID_Emp_Info, font=('arial', 20, 'bold'), textvariable=Work_Order,   fg="black", bd=5).grid(row=1, column=1, sticky=W)
